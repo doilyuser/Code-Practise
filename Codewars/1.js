fbd976ded5414af1a83216097b3b56ac5e8eb52d
Louis Dyer <louisalexanderdyer@gmail.com>
2023-05-19 20:49:07 +1200

    Added solution

--- Codewars/1.js
+++ Codewars/1.js
@@ -17,24 +17,49 @@
 // Inputs are guaranteed to be a single string
 
 export default function isValidIP(str) {
-  return false
+  // check for exactly four octets
+  const arr = str.split('.')
+  if (arr.length != 4) return false
+
+  // check for spaces, letters & line breaks
+  const regex = / |[a-zA-Z]|\n/
+  if (str.match(regex)) return false
+
+  for (let i = 0; i < arr.length; i++) {
+    // check number is between 0 & 255
+    if ((arr[i] < 0) | (arr[i] > 255)) return false
+    // check number has no leading zeros
+    if (Number(arr[i]).toString() !== arr[i]) return false
+  }
+
+  return true
 }
 
-console.log('true: ' + isValidIP('0.0.0.0'))
-console.log('true: ' + isValidIP('12.255.56.1'))
-console.log('true: ' + isValidIP('137.255.156.100'))
-console.log('false: ' + isValidIP(''))
-console.log('false: ' + isValidIP('abc.def.ghi.jkl'))
-console.log('false: ' + isValidIP('123.456.789.0'))
-console.log('false: ' + isValidIP('12.34.56'))
-console.log('false: ' + isValidIP('01.02.03.04'))
-console.log('false: ' + isValidIP('256.1.2.3'))
-console.log('false: ' + isValidIP('1.2.3.4.5'))
+// SOLUTION BY: ryanzyy
+// function isValidIP(str) {
+//   return /^(([1-9]?\d|1\d\d|2[0-4]\d|25[0-5])(\.(?!$)|$)){4}$/.test(str);
+// }
 
-// assert.strictEqual(isValidIP('123,45,67,89'   ), false);
-// assert.strictEqual(isValidIP('1e0.1e1.1e2.2e2'), false);
-// assert.strictEqual(isValidIP(' 1.2.3.4'       ), false);
-// assert.strictEqual(isValidIP('1.2.3.4 '       ), false);
-// assert.strictEqual(isValidIP('12.34.56.-7'    ), false);
-// assert.strictEqual(isValidIP('1.2.3.4\n'      ), false);
-// assert.strictEqual(isValidIP('\n1.2.3.4'      ), false);
+// SOLUTION BY: smrg
+// const isValidIP = require('net').isIPv4;
+
+// TESTS:
+
+// console.log('true: ' + isValidIP('0.0.0.0'))
+// console.log('true: ' + isValidIP('1.2.3.4'))
+// console.log('true: ' + isValidIP('12.255.56.1'))
+// console.log('true: ' + isValidIP('137.255.156.100'))
+// console.log('false: ' + isValidIP(''))
+// console.log('false: ' + isValidIP('abc.def.ghi.jkl'))
+// console.log('false: ' + isValidIP('123.456.789.0'))
+// console.log('false: ' + isValidIP('12.34.56'))
+// console.log('false: ' + isValidIP('01.02.03.04'))
+// console.log('false: ' + isValidIP('256.1.2.3'))
+// console.log('false: ' + isValidIP('1.2.3.4.5'))
+// console.log('false: ' + isValidIP('123,45,67,89'))
+// console.log('false: ' + isValidIP('1e0.1e1.1e2.2e2'))
+// console.log('false: ' + isValidIP(' 1.2.3.4'))
+// console.log('false: ' + isValidIP('1.2.3.4 '))
+// console.log('false: ' + isValidIP('12.34.56.-7'))
+// console.log('false: ' + isValidIP('1.2.3.4\n'))
+// console.log('false: ' + isValidIP('\n1.2.3.4'))
