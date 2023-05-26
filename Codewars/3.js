d2d8a9e85fab933aa1411847fca0521b2b1fe3cf
Louis Dyer <louisalexanderdyer@gmail.com>
2023-05-26 14:57:09 +1200

    Fix typo

--- Codewars/3.js
+++ Codewars/3.js
@@ -4,17 +4,14 @@
 
 export default function rgb(r, g, b) {
   if (r < 0) r = 0
-  if (b < 0) b = 0
+  if (g < 0) g = 0
   if (b < 0) b = 0
   if (r > 255) r = 255
   if (g > 255) g = 255
   if (b > 255) b = 255
-  if (r < 10) r = `0${r}`
-  if (g < 10) g = `0${g}`
-  if (b < 10) b = `0${b}`
-  const rHex = r.toString(16).toUpperCase()
-  const gHex = g.toString(16).toUpperCase()
-  const bHex = b.toString(16).toUpperCase()
+  const rHex = r.toString(16).toUpperCase().padStart(2, '0')
+  const gHex = g.toString(16).toUpperCase().padStart(2, '0')
+  const bHex = b.toString(16).toUpperCase().padStart(2, '0')
   return `${rHex}${gHex}${bHex}`
 }
 
@@ -27,3 +24,5 @@ console.log(rgb(255, 255, 300) + ' = FFFFFF')
 console.log(rgb(0, 0, 0) + ' = 000000')
 console.log(rgb(148, 0, 211) + ' = 9400D3')
 console.log(rgb(1, 0, 9) + ' = 010009')
+console.log(rgb(223, 242, 15) + ' = DFF20F')
+console.log(rgb(139, -1, 241) + ' = 8B00F1')
