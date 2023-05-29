b1a868c22e0d8fea151e671922764e15a52a1970
Louis Dyer <louisalexanderdyer@gmail.com>
2023-05-29 14:36:30 +1200

    Added other user solutions

--- Codewars/4.js
+++ Codewars/4.js
@@ -6,12 +6,44 @@
 // Only worry about the following format:
 // (123) 456-7890 (don't forget the space after the close parentheses)
 
+// My Solution:
+
 export default function validPhoneNumber(phoneNumber) {
   const regex = /^[(][0-9]{3}[)][\s][0-9]{3}[-][0-9]{4}$/
   return regex.test(phoneNumber) ? true : false
 }
 
+// Tests:
+
 console.log(validPhoneNumber('(123) 456-7890') + ' = true')
 console.log(validPhoneNumber('123 456-7890') + ' = false')
 console.log(validPhoneNumber('(1111)555 2345') + ' = false')
 console.log(validPhoneNumber('(098) 123 4567') + ' = false')
+
+// Other solutions online:
+
+// function validPhoneNumber(phoneNumber){
+//   return /^\(\d+\)\s\d{3}-\d{4}$/.test(phoneNumber);
+// }
+
+// function validPhoneNumber(phoneNumber){
+//   return /^\(\d{3}\) \d{3}\-\d{4}$/.test(phoneNumber);
+// }
+
+// function validPhoneNumber(str) {
+
+//   if (str.length !== 14) {
+//     return false;
+//   }
+//   if (str[4] !== ")") {
+//     return false;
+//   }
+//   if (str[5] !== " ") {
+//     return false;
+//   }
+//   if (str[9] !== "-") {
+//     return false;
+//   } else {
+//     return true;
+//   }
+// }
