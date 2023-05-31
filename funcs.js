b080d07de1990856f39c9bd79365b9258df4937c
Louis Dyer <louisalexanderdyer@gmail.com>
2023-05-31 16:03:42 +1200

    Add new problem and solution

--- funcs.js
+++ funcs.js
@@ -2,7 +2,8 @@ import isValidIP from './Codewars/1.js'
 import parse from './Codewars/2.js'
 import rgb from './Codewars/3.js'
 import validPhoneNumber from './Codewars/4.js'
+import isPrime from './Codewars/5.js'
 
 import dailyCode from './DCP/1.js'
 
-export { isValidIP, parse, rgb, validPhoneNumber }
+export { isValidIP, parse, rgb, validPhoneNumber, isPrime }
