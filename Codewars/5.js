6bd51b1f03e76d452f081ebcab617e8e8ee87c05
Louis Dyer <louisalexanderdyer@gmail.com>
2023-06-06 13:20:36 +1200

    refactor

--- Codewars/5.js
+++ Codewars/5.js
@@ -14,11 +14,11 @@ export default function isPrime(num) {
   return num > 1
 }
 
-console.log(isPrime(0), false, '0 is not prime')
-console.log(isPrime(1), false, '1 is not prime')
-console.log(isPrime(2), true, '2 is prime')
-console.log(isPrime(73), true, '73 is prime')
-console.log(isPrime(75), false, '75 is not prime')
-console.log(isPrime(-1), false, '-1 is not prime')
-console.log(isPrime(9), false, '9 is not prime')
-console.log(isPrime(7), true, '7 is prime')
+// console.log(isPrime(0), false, '0 is not prime')
+// console.log(isPrime(1), false, '1 is not prime')
+// console.log(isPrime(2), true, '2 is prime')
+// console.log(isPrime(73), true, '73 is prime')
+// console.log(isPrime(75), false, '75 is not prime')
+// console.log(isPrime(-1), false, '-1 is not prime')
+// console.log(isPrime(9), false, '9 is not prime')
+// console.log(isPrime(7), true, '7 is prime')
