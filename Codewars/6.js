668c94ca2310bfe5efa39bbd80fe71d29ff1acbd
Louis Dyer <louisalexanderdyer@gmail.com>
2023-06-26 14:40:30 +1200

    Comment out tests

--- Codewars/6.js
+++ Codewars/6.js
@@ -36,11 +36,11 @@ export function decode(string) {
 
 // Tests:
 
-console.log(encode('hello'), ' = h2ll4')
-console.log(encode('HELLO'), ' = H2LL4')
-console.log(encode('How are you today?'), ' = H4w 1r2 y45 t4d1y?')
-console.log(encode('This is an encoding test.'), ' = Th3s 3s 1n 2nc4d3ng t2st.')
-console.log(decode('h2ll4'), ' = hello')
+// console.log(encode('hello'), ' = h2ll4')
+// console.log(encode('HELLO'), ' = H2LL4')
+// console.log(encode('How are you today?'), ' = H4w 1r2 y45 t4d1y?')
+// console.log(encode('This is an encoding test.'), ' = Th3s 3s 1n 2nc4d3ng t2st.')
+// console.log(decode('h2ll4'), ' = hello')
 
 // Other solution online:
 
